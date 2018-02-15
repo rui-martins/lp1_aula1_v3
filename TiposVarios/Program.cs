7163919e467241f4c07b987b130189be661523ec
rui-martins <vandroste@gmail.com>
2018-02-15 17:28:02 +0000

    Ex4 - Verbatim

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -10,6 +10,12 @@ namespace TiposVarios
     {
         static void Main(string[] args)
         {
+            string tipo1 = "Esta é a primeira string";
+            string tipo2 = @"C:\Users\Rui\Documents\Visual Studio 2017";
+            string tipo3 = "\"Esta string esta' entre aspas\"";
+            string tipo4 = @"""Esta string esta' entre aspas""";
+
+            Console.WriteLine("\n" + tipo1 + "\n" + tipo2 + "\n" + tipo3 + "\n" + tipo4);
         }
     }
 }
